4cc8e804c82c342f48911114b7a148f1411d2a85
murat <murat_yildiz@gmx.net>
2019-09-10 23:08:54 +0200

    Replaced Jasmin with Cucumber. Simplified sample site. Created sample cucumber feature

---
+++ e2e/src/pages/sample.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get(browser.baseUrl) as Promise<any>;
+  }
+
+  getTitleText() {
+    return element(by.css('div.terminal pre')).getText() as Promise<string>;
+  }
+}
